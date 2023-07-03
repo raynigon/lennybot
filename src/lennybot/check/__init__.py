e7e2d374ccd953e8c46be3077823e6cf93e221e3
dbalucas <lucas.duerksen.work@gmail.com>
2023-07-03 15:27:02 +0200

    line too long + trailing white spaces

--- src/lennybot/check/__init__.py
+++ src/lennybot/check/__init__.py
@@ -5,5 +5,10 @@ from .icheck import ICheck
 def create_check(name, source_version, latest_version, config) -> ICheck:
     check_type = config.type
     if check_type == "docker-image-available":
-        return DockerImageAvailableCheck(name, source_version, latest_version, config)
+        return DockerImageAvailableCheck(
+            name,
+            source_version,
+            latest_version,
+            config
+            )
     raise Exception(f"Unknown Check Type: {check_type}")
