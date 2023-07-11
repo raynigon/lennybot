9cf9b48fc0ca5496d90997b8a0a627a60965c69f
dbalucas <lucas.duerksen.work@gmail.com>
2023-07-11 18:03:34 +0200

    Implement Image check logic with generic image

--- src/lennybot/check/docker_image_available.py
+++ src/lennybot/check/docker_image_available.py
@@ -1,11 +1,18 @@
 import logging
+import re
+from typing import Optional
 
 import requests
 
 from ..config.config import LennyBotCheckConfig
 from .icheck import ICheck
 
-# import subprocess
+
+class DockerImage:
+    def __init__(self, registry: str, name: str, tag: Optional[str] = None) -> None:
+        self._registry = registry
+        self._name = name
+        self._tag = tag
 
 
 class DockerImageAvailableCheck(ICheck):
@@ -32,10 +39,45 @@ class DockerImageAvailableCheck(ICheck):
         """
         Checks if an image exists in the remote registry from _image_pattern.
         """
-        image_path = self._image_pattern.replace("{{version}}", self.target_version)
-        image_url = f"https://{image_path}"
+        image = self._parse_image()
 
-        res = requests.get(image_url)
+        if image._registry is None:
+            return self._exists_on_docker_hub(image)
+
+        return self._exists_on_registry(image)
+
+    def _parse_image(self):
+        if ":" not in self._image_pattern:
+            raise Exception("Image pattern does not contain a tag seperator")
+
+        image_name = self._image_pattern.split(":")[0]
+        image_tag = self._image_pattern.split(":")[1].replace("{{version}}", self.target_version)
+
+        pattern = (
+            r"(?:([\-\_\.\w]+)$)|(?:([\-\_\.\w]+)/([\-\_\.\w]+)$)|(?:([\-\.A-z0-9]+)/([\-\_\.\w]+)/([\-\_\.\w]+)$)"
+        )
+
+        match = re.match(pattern, image_name)
+        print(match, match.group(1))
+        if match.group(1) is not None:
+            return DockerImage(None, match.group(1), image_tag)
+        elif match.group(2) is not None:
+            return DockerImage(None, match.group(2) + "/" + match.group(3), image_tag)
+        else:
+            return DockerImage(match.group(4), match.group(5) + "/" + match.group(6), image_tag)
+
+    def _exists_on_docker_hub(self, image: DockerImage):
+        res = requests.get(f"https://hub.docker.com/v2/repositories/{image._name}/tags")
+        if res.status_code != 200:
+            raise Exception("Unexpected status")
+
+        for tag in res.json()["results"]:
+            if tag["name"] == image._tag:
+                return True
+        return False
+
+    def _exists_on_registry(self, image: DockerImage):
+        res = requests.get(f"https://{image._registry}/v2/{image._name}/manifests/{image._tag}")
         if res.status_code == 200:
             return True
         elif res.status_code == 404:
