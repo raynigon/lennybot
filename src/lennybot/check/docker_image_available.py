3f7965769aafd97832c35b27034d47018a5a24fa
dbalucas <lucas.duerksen.work@gmail.com>
2023-08-25 09:55:32 +0200

    fix unauthentication logic and acces token gen

--- src/lennybot/check/docker_image_available.py
+++ src/lennybot/check/docker_image_available.py
@@ -114,20 +114,25 @@ class DockerImageAvailableCheck(ICheck):
             "access_type": "offline",
         }
 
-        if registry in self._container_config.registries.keys():
-            registry_data = self._container_config.registries[registry]
-            params["password"] = registry_data.password
-            params["username"] = registry_data.username
+        registry_data = self._container_config.registries[registry]
+        password = registry_data.password
+        username = registry_data.username
 
         url = f"{authentication_header.realm}?{urlencode(params)}"
-        response = requests.get(url)
+        response = requests.get(url, auth=(username, password))
 
         if response.status_code == 200:
             token_data = response.json()
+
             access_token = token_data.get("token")
+
+            if token_data.get("token") is None:
+                access_token = token_data.get("access_token")
+
             return str(access_token)
 
         if response.status_code == 401:
+            logging.debug("error during authentication:", response.status_code, response.headers)
             raise Exception("Error occured: Unauthenticated: ", response.status_code)
 
         raise Exception("Unexpected Status Code", response.status_code)
