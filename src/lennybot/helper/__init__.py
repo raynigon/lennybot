7d6da98c80be713899963f5b015671cbb5afbbf3
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2024-07-01 09:31:17 +0200

    Increase Version Multiplier

--- src/lennybot/helper/__init__.py
+++ src/lennybot/helper/__init__.py
@@ -4,5 +4,5 @@ def semver_2_vc(version):
     parts = version.split(".")
     version_code = 0
     for part in parts:
-        version_code = version_code * 100 + int(part)
+        version_code = version_code * 1000 + int(part)
     return version_code
