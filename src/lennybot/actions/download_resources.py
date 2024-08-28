9db14d402c129ad78a61db49e70005139df4e12e
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2024-08-28 07:34:59 +0200

    Improve the error message for the resource download

--- src/lennybot/actions/download_resources.py
+++ src/lennybot/actions/download_resources.py
@@ -1,11 +1,13 @@
 import requests
+import logging
 
 from ..config.config import LennyBotActionConfig
 from .iaction import IAction
 
 
-class DownloadResourcesAction(IAction):
+class DownloadResourceAction(IAction):
     def __init__(self, name, source_version, target_version, config: LennyBotActionConfig) -> None:
+        self._log = logging.getLogger(self.__class__.__name__)
         self._name = name
         self._source_version = source_version
         self._target_version = target_version
@@ -30,8 +32,12 @@ class DownloadResourcesAction(IAction):
 
     def run(self):
         download_url = self._url.replace("{{version}}", self._target_version)
+        self._log.debug("Downloading resource from %s to %s", download_url, self._target_path)
         response = requests.get(download_url)
         if response.status_code != 200:
-            raise Exception("Unable to download resources")
+            self._log.error("Unable to download resource, received status code: %d\n%s", response.status, response.text)
+            raise Exception("Unable to download resource, received status code: " + str(response.status_code))
+        self._log.debug("Downloaded resources successfully")
         with open(self._target_path, "w", encoding="utf-8") as file_ptr:
             file_ptr.write(response.text)
+        self._log.debug("Saved resource to %s", self._target_path)
